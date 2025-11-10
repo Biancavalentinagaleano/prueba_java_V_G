084e568528d7a997248c708feb26d5467d59791b
Biancavalentinagaleano <valentiespindola6@gmail.com>
2025-11-10 18:50:58 -0300

    Update ejemploscript.js

--- ejemploscript.js
+++ ejemploscript.js
@@ -1,3 +1,11 @@
-alert("Bienvenido a la prueba JavaScript");
+//alert("Bienvenido a la prueba JavaScript");//
 
-document.querySelector("h1").textContent = "¡ESTO INDICA QUE FUNCIONA JAVASCRIPT!";
+//(document.querySelector("h1").textContent = "¡ESTO INDICA QUE FUNCIONA JAVASCRIPT!";//
+
+alert("Bienvenido a la prueba JavaScript");
+let userName = prompt("Para personalizar tu experiencia, por favore indica tu nombre: ");
+let welcomeText = ¡Bienvenido/a de nuevo, ${userName}!;
+alert(welcomeText); 
+
+
+
